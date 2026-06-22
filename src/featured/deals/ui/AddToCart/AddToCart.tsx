9979ecd33dd53124079c8aadcf42926fb9ac58ef
SergWebAcceptance <serg.web@acceptance.ltd>
2026-06-22 14:08:27 +0300

    Enhance AddToCart component to redirect to checkout page after adding an item to the cart. Update Deals module styles for improved visual hierarchy and button design.

--- src/featured/deals/ui/AddToCart/AddToCart.tsx
+++ src/featured/deals/ui/AddToCart/AddToCart.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
 import { Button } from '@/shared/ui/kit';
 
 import { useCartStore } from '@/featured/cart/model/store';
@@ -18,7 +20,7 @@ export const AddToCart = ({
   button_text: string;
 }) => {
   const { addToCart, setTotal, total, setIsCartFilled } = useCartStore();
-
+  const router = useRouter();
   const handleAddToCart = () => {
     addToCart({
       id: id.toString(),
@@ -30,6 +32,7 @@ export const AddToCart = ({
     });
     setTotal(total + price);
     setIsCartFilled(true);
+    router.push('/checkout');
   };
 
   return <Button text={button_text} type="button" onClick={handleAddToCart} />;
